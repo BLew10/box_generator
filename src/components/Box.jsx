b1edab6320f8b886d7c583e05b48d72b483ab93f
BLew10 <Brandonlewis.10@gmail.com>
2022-10-25 15:52:01 -0700

    initial commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './component.module.css';
+
+
+const Box = () => {
+    const [box, setBox] = useState("")
+    return (
+        <div className={styles.box}>
+            
+        </div>
+    )
+}
+
+export default Box
---
+++ src/components/ColorForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ColorForm = () => {
+    return (
+        <div>
+            <form>
+                <label id='colorPicker'>Color: </label>
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+
+export default ColorForm
